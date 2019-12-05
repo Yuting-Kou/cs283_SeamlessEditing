f8e90d2ab767dfe130a7f6331fded58548f88d34
kou yuting <yutingkou@outlook.com>
2019-12-05 01:12:55 -0500

    Move test code from gradient into test

--- src/gradient_field.py
+++ src/gradient_field.py
@@ -143,24 +143,3 @@ class Gradient_Field:
         self.cur_method = self.method_list[3]
         pass
 
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-
-    destination = plt.imread(r'../test/swan.jpg')
-    source = plt.imread(r'../test/kyt.jpg')
-    print(source.shape, destination.shape)
-
-    mask = plt.imread(r'../test/mask.png')
-    mask[mask != 1] = 0
-    mask = mask.astype(np.uint8)[:, :, 0]
-
-    test = Gradient_Field(source=source, destination=destination, mask=mask, offset=[-136, 50])
-    A, b = test.get_v()
-
-    # plt.figure(figsize=(10, 5))
-    # plt.subplot(1, 2, 1)
-    # plt.imshow(test.f_star)
-    # plt.subplot(1, 2, 2)
-    # plt.imshow(test.g)
-    # plt.show()
