56fbe92c6b9f7ba36a5a18c1dd9417cbeb79c663
kou yuting <yutingkou@outlook.com>
2019-12-05 14:11:36 -0500

    Comment out the drawer part

--- test/test.py
+++ test/test.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from src.gradient_field import Gradient_Field
-from src.mask import MaskPainter
 
 if __name__ == '__main__':
     paths = '../test/'
@@ -10,15 +9,19 @@ if __name__ == '__main__':
     source = 'kyt.jpg'
     save = 'mask.png'
 
-    mp = MaskPainter(g_impath=paths + source, f_impath=paths + destination)
-    tx, ty, savepath = mp.paint_mask(maskname=save)
-    print('save mask in' + savepath)
+    # mp = MaskPainter(g_impath=paths + source, f_impath=paths + destination)
+    # tx, ty, savepath = mp.paint_mask(maskname=save)
+    # print('save mask in' + savepath)
+    # print('offset=({0},{1})'.format(tx,ty))
+    source = plt.imread(source)  # source=mp.image_g
+    destination = plt.imread(destination)  # destination=mp.image_f
 
+    tx, ty = (-184, -158)
     mask = plt.imread(paths + save)
     mask[mask != 1] = 0
     mask = mask.astype(np.uint8)[:, :, 0]  # only 1 channel mask
 
-    test = Gradient_Field(source=mp.image_g, destination=mp.image_f, mask=mask, offset=[tx, ty],
+    test = Gradient_Field(source=source, destination=destination, mask=mask, offset=[tx, ty],
                           neighbor_ker=np.ones((5, 5)))
 
     print('Choose methods from:', test.print_methods())
