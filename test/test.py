f74a1f9e1d70ed32d598ab93499f5707a26f102c
kou yuting <yutingkou@outlook.com>
2019-12-09 01:42:42 -0500

    Enable resize

--- test/test.py
+++ test/test.py
@@ -57,28 +57,4 @@ if __name__ == '__main__':
     x1 = sor_solver_jit(data, rows, diag, b, omega, x0)
     plt.imshow(test.combine(x1))
     plt.show()
-    # print('It costs {:.2f} sec.'.format(time.time() - begin))
-    # begin = time.time()
-    # test1 = Poisson_system(source=source, destination=destination, mask=mask, offset=[tx, ty])
-    # A1, b1 = test1.get_Ab(method='dg')
     print('It costs {:.2f} sec.'.format(time.time() - begin))
-
-    # print('Choose methods from:', test.print_methods())
-    #
-
-    # x = sor_solver(A, b[0], 1.5, np.zeros(A.shape[0]), 1e-6)
-
-    #
-    # print(A.shape, b.shape)
-    # # do not need to calculate the same thing
-    # # A1, b1 = test.get_v(method='dgf')
-    # # # calculate new methods.
-    # # A2, b2 = test.get_v(method='dg')
-    # # A3, b3 = test.get_v(method='Mdg', new_mask=np.ones_like(test.g))
-    #
-    # g=test.g.copy()
-    # g[test.boundary == 0]=1
-    # plt.imshow(g)
-    # plt.show()
-    # plt.imshow(test.g)
-    # plt.show()
